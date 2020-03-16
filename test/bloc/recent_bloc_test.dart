083d55a621bcc385cc8efd94eb66b5f0620c8743
Aldila Iskandar <aldila.iskndr@gmail.com>
2020-03-16 15:34:09 +0700

    [UPDATE][FEATURE] improve bloc recent is able to delete all

--- test/bloc/recent_bloc_test.dart
+++ test/bloc/recent_bloc_test.dart
@@ -12,7 +12,10 @@ void main() {
     mockSpendRepository = MockSpendRepository();
   });
   group('Recent BLoC Test', () {
-    final List<Spend> spends = [Spend(description: 'beli bakso', cost: 10000)];
+    final List<Spend> spends = [
+      Spend(description: 'beli bakso', cost: 10000),
+      Spend(description: 'beli bensin', cost: 15000),
+    ];
     blocTest('Initial state', build: () async {
       print('masuk?');
       return RecentBloc(mockSpendRepository);
@@ -26,5 +29,19 @@ void main() {
         },
         act: (RecentBloc bloc) async => bloc.add(GetRecentEvent()),
         expect: [RecentDataState(spends)]);
+
+    blocTest('Delete All Recent Data',
+        build: () async {
+          when(mockSpendRepository.getAllSpend())
+              .thenAnswer((_) async => Future<List<Spend>>.value(spends));
+          return RecentBloc(mockSpendRepository);
+        },
+        act: (RecentBloc bloc) async {
+          bloc.add(GetRecentEvent());
+          bloc.add(DeleteAllRecent());
+        },
+        expect: [RecentDataState(spends)],
+        verify: (_) async => verify(mockSpendRepository.deleteAll()).called(1)
+        );
   });
 }
